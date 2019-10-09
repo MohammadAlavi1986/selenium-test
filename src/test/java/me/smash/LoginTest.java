b4fbe68e40572f4805317f1837198984435e5d7c
mohammad.alavi@aurea.com <mohammad.alavi@aurea.com>
2019-10-09 17:45:21 +0300

    test

--- src/test/java/me/smash/LoginTest.java
+++ src/test/java/me/smash/LoginTest.java
@@ -2,17 +2,26 @@ package me.smash;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-@Test(enabled = false)
 public class LoginTest {
 
   private WebDriver webDriver;
@@ -25,8 +34,20 @@ public class LoginTest {
   private WebElement btnLogin;
 
   @BeforeTest
-  public void setUp() {
-    webDriver = new ChromeDriver();
+  @Parameters("browser")
+  public void setUp(String browser) {
+    switch (browser) {
+      case "firefox":
+        webDriver = new FirefoxDriver();
+        break;
+      case "chrome":
+        webDriver = new ChromeDriver();
+        break;
+      default:
+        throw new IllegalArgumentException("unsupported browser '" + browser + "'");
+    }
+
+    webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     webDriver.get("http://demo.guru99.com/v4/");
 
     PageFactory.initElements(webDriver, this);
@@ -38,9 +59,14 @@ public class LoginTest {
   }
 
   @Test
-  public void test1() {
+  public void test1() throws IOException {
     username.sendKeys("mngr227463");
     password.sendKeys("ajAzavU");
+
+    File screenshot =((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+    try(FileOutputStream fos = new FileOutputStream("screenshot.png")) {
+      Files.copy(Paths.get(screenshot.getAbsolutePath()), fos);
+    }
     btnLogin.submit();
 
     assertThat(webDriver.findElement(By.cssSelector(".heading3 > td")).getText())
