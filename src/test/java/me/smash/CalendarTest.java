af7dede71587eb847a84ae11fd9d0df9655caee0
mohammad.alavi@aurea.com <mohammad.alavi@aurea.com>
2019-10-09 15:43:21 +0300

    test

--- src/test/java/me/smash/CalendarTest.java
+++ src/test/java/me/smash/CalendarTest.java
@@ -27,6 +27,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+@Test(enabled = false)
 public class CalendarTest {
 
   private WebDriver driver;
