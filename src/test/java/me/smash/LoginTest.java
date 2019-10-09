8b5e756b02265b032360ef0f9d14bdb9bed6cbc7
mohammad.alavi@aurea.com <mohammad.alavi@aurea.com>
2019-10-09 16:08:28 +0300

    test

--- src/test/java/me/smash/LoginTest.java
+++ src/test/java/me/smash/LoginTest.java
@@ -12,6 +12,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+@Test(enabled = false)
 public class LoginTest {
 
   private WebDriver webDriver;
